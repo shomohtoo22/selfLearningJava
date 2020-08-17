d3c95eda104852160f600359df81b83e5e343ec0
shomohtoo22 <66963686+shomohtoo22@users.noreply.github.com>
2020-08-17 12:07:15 -0500

    Update Main.java
    add some sortedSet examples.

--- collections/set/Main.java
+++ collections/set/Main.java
@@ -1,4 +1,5 @@
 import java.util.Set;
+import java.util.SortedSet;
 
 public class Main {
 
@@ -9,8 +10,10 @@ public class Main {
         Product banana = new Product("Banana", 3);
         Product grape = new Product("Grape", 5);
 
+
         Basket basket = new Basket();
 
+        // add products in sets
         basket.addProduct(banana);
         basket.addProduct(grape);
         basket.addProduct(apple);
@@ -21,18 +24,36 @@ public class Main {
         basket.addProduct(new Product("Grape", 5));
 
         // product should not be duplicate and ordered.
-        Set<Product> products = basket.getProducts();
+        Set<Product> setProducts = basket.getSetProducts();
+
+        // product should be sorted by id
+        SortedSet<Product> sortedSetProduct = basket.getSortedSetProductsProducts();
 
         // product should not be duplicated and should be ordered by name
         Set<Product> treeSetProducts = basket.getTreeSetProductsProducts();
 
-        for(Product product : products) {
+        System.out.println("Regular set");
+        for(Product product : setProducts) {
             System.out.println(product);
         }
-        System.out.println("--------------------");
+
+        System.out.println("\nSortedSet");
+        for(Product product : sortedSetProduct) {
+            System.out.println(product);
+        }
+        // working with tailSet. Should return everything including passed argument
+        SortedSet<Product> tailSet = sortedSetProduct.tailSet(apple);
+        System.out.println(tailSet);
+        // working with headSet. Should return everything up to passed argument(Exclusive)
+        SortedSet<Product> headSet = sortedSetProduct.headSet(apple);
+        System.out.println(headSet);
+
+        System.out.println("\nTreeSet");
         for(Product product : treeSetProducts) {
             System.out.println(product);
         }
+
+
 
     }
 }
