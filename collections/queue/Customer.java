09b094222b2e778470c7afaf817219cd61f333e7
shomohtoo22 <66963686+shomohtoo22@users.noreply.github.com>
2020-08-17 17:31:42 -0500

    Create Customer.java

---
+++ collections/queue/Customer.java
@@ -0,0 +1,17 @@
+public class Customer {
+
+    public static Customer ANNA = new Customer("Anna");
+    public static Customer JACK = new Customer("Jack");
+    public static Customer TOM = new Customer("Tom");
+    public static Customer EDDY = new Customer("Eddy");
+
+    private final String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void reply(String message) {
+        System.out.printf("Hey %s: %s\n", name, message);
+    }
+}
