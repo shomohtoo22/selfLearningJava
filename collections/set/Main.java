cbbc256e0b58e585b785640a17cd6cc46638ed52
shomohtoo22 <66963686+shomohtoo22@users.noreply.github.com>
2020-08-17 11:09:07 -0500

    Update Main.java

--- collections/set/Main.java
+++ collections/set/Main.java
@@ -11,18 +11,26 @@ public class Main {
 
         Basket basket = new Basket();
 
+        basket.addProduct(banana);
+        basket.addProduct(grape);
         basket.addProduct(apple);
         basket.addProduct(orange);
         basket.addProduct(pear);
-        basket.addProduct(banana);
-        basket.addProduct(grape);
 
         // this will add grape if hashcode and equals are not override in product class
         basket.addProduct(new Product("Grape", 5));
 
+        // product should not be duplicate and ordered.
         Set<Product> products = basket.getProducts();
 
+        // product should not be duplicated and should be ordered by name
+        Set<Product> treeSetProducts = basket.getTreeSetProductsProducts();
+
         for(Product product : products) {
+            System.out.println(product);
+        }
+        System.out.println("--------------------");
+        for(Product product : treeSetProducts) {
             System.out.println(product);
         }
 
