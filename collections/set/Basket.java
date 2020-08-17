3d5aae7815965d9e0cee21d69174c4fe98db2f86
shomohtoo22 <66963686+shomohtoo22@users.noreply.github.com>
2020-08-17 11:09:47 -0500

    Update Basket.java

--- collections/set/Basket.java
+++ collections/set/Basket.java
@@ -1,18 +1,25 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Basket {
 
     private final Set<Product> products = new HashSet<>();
+    private final Set<Product> treeSetProducts = new TreeSet<>(Product.BY_NAME);
 
 
     public Basket(){}
 
     public void addProduct(Product product) {
         products.add(product);
+        treeSetProducts.add(product);
     }
 
     public Set<Product> getProducts() {
         return products;
+    }
+
+    public Set<Product> getTreeSetProductsProducts() {
+        return treeSetProducts;
     }
 }
